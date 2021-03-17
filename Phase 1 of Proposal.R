6e24b995369c32bc3e5035f4fe37595b82836159
Noel Asmann <nasmann@utexas.edu>
2021-03-17 18:47:06 -0500

    hello simone

--- Phase 1 of Proposal.R
+++ Phase 1 of Proposal.R
@@ -15,3 +15,19 @@ cleanres = cleanres %>% drop_na()
 CNAME <- Results %>% group_by(COUNTY_NM) %>% summarise(count = n())
 #looking at one county to see if it had any periphyton and nitrogen data - nope
 blaht <- Results %>% filter(COUNTY_NM == 'Alameda County')
+#might be easier to do this if i separate them into two different data frames
+periphyton <- cleanres %>% filter(PARM_NM == 'Periphyton biomass')
+nitrogenp <- cleanres %>% filter(PARM_NM == 'Total nitrogen, wf')
+#find the different counties in each
+nperi <- periphyton %>% group_by(COUNTY_NM) %>% summarise(count = n())
+nnitr <- nitrogenp %>% group_by(COUNTY_NM) %>% summarise(count = n())
+# simular - cobb, fairfax , franklin
+
+#matching the variables - from mohanad
+cleanres2 <- Results %>% group_by(SITE_NO, PARM_NM) %>% summarize(median_val = median(RESULT_VA)) %>% ungroup()
+data <- pivot_wider(cleanres2,id_cols = SITE_NO, names_from = PARM_NM, values_from = median_val) %>% 
+  select(c(1,4,5)) %>% na.omit()
+
+
+
+
